6ef395941ff1e5b46828ee8190fac97eca8e53a7
winpzs <winpzs@gmail.com>
2016-10-20 10:22:46 +0800

    fix npm start error

--- app/main-aot.ts
+++ app/main-aot.ts
@@ -1,7 +1,3 @@
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-// import { AppModule } from './app.module';
-// const platform = platformBrowserDynamic();
-// platform.bootstrapModule(AppModule);
 
 import { platformBrowser }    from '@angular/platform-browser';
 
